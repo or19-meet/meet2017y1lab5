b07de1713f026f5f9eb6128307c046fc7231f4fe
or19-meet <or19@meet.mit.edu>
2017-07-30 10:55:03 +0200

    Stopping here

---
+++ fun2.py
@@ -0,0 +1,15 @@
+#print ("*"*5)
+#print("*")
+#print("*")
+#print("*")
+#print("*")
+#print("*")
+
+#def draw_1d(n, char):
+ #   print(char*n)
+
+def draw_rectangle(n1, n2):
+    for i in range(n1):
+        print('*'*n2)
+        
+
